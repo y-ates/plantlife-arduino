2bd5e700c56c5ba2fd5894a1f59ca56b2a95ccf4
Yakup Ates <Yakup.Ates@rub.de>
2017-09-14 09:02:03 +0200

    correct comment

--- pump.cpp
+++ pump.cpp
@@ -69,6 +69,7 @@ void loop() {
  */
 boolean decidePump() {
     if (isSunrise) {  // sunrise
+    if (isSundown) {  // night
         if (humidity_ground > humidityGroundThreshold) {
             if (hasWater) {
                 return true;
